f81ea64e34996a15f54ea970392e65b18d9ff2e5
Ashwin.D.S <astroashwin@outlook.com>
2022-04-10 07:26:47 +0000

    fixup

--- mirror/__main__.py
+++ mirror/__main__.py
@@ -10,6 +10,6 @@ for repo in repos:
     # https://username:your_password@github.com/username/repo_name.git
     command = "rm -rf temp\ngit clone --mirror " + \
         repo[0] + " temp \ncd temp \ngit push -f https://geek0609:" + PAT + "@gitlab.com/PixelOS-Devices/" + \
-        repo[1] + ".git --mirror\ncd .\n"
+        repo[1] + ".git --mirror\ncd ..\n"
     print(command)
     file.write(command)
