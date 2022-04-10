713e19851ebfb2031570ab993b55bb3fa25e5b26
Ashwin.D.S <astroashwin@outlook.com>
2022-04-10 07:25:44 +0000

    add more repos to test

--- mirror/repoList.py
+++ mirror/repoList.py
@@ -1,4 +1,6 @@
 repos = [
     ["https://github.com/PixelOS-Devices/vendor_xiaomi", "vendor_xiaomi"],
-    ["https://github.com/Neternels/android_kernel_xiaomi_mojito", "kernel_xiaomi_mojito"]
+    ["https://github.com/Neternels/android_kernel_xiaomi_mojito", "kernel_xiaomi_mojito"],
+    ["https://github.com/ArrowOS-Devices/android_vendor_miuicamera", "vendor_miuicamera"],
+    ["https://github.com/EmanuelCN/kernel_xiaomi_sm8250", "kernel_xiaomi_sm8250"]
 ]
