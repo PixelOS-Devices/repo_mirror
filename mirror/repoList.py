66ae7146deb535caf823d1b38d6e735e135687be
sajidshahriar72543 <sazidshahriar39@gmail.com>
2022-04-11 11:45:47 +0600

    beryllium: Add kernel and vendor tree

--- mirror/repoList.py
+++ mirror/repoList.py
@@ -3,4 +3,6 @@ repos = [
     ["https://github.com/Neternels/android_kernel_xiaomi_mojito", "kernel_xiaomi_mojito"],
     ["https://github.com/ArrowOS-Devices/android_vendor_miuicamera", "vendor_miuicamera"],
     ["https://github.com/EmanuelCN/kernel_xiaomi_sm8250", "kernel_xiaomi_sm8250"]
+    ["https://github.com/PainKiller3/vendor_xiaomi", "vendor_xiaomi_beryllium_sdm845"]
+    ["https://github.com/PainKiller3/kernel_xiaomi_sdm845", "kernel_xiaomi_sdm845"]
 ]
